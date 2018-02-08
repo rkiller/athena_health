e25e0294b78fe68e2d8917410597a889dddb1c6e
rkiller <rogerkiller@gmail.com>
2018-02-07 16:49:05 -0800

    Moving COF methods to Payments

--- lib/athena_health/endpoints/payments.rb
+++ lib/athena_health/endpoints/payments.rb
@@ -2,6 +2,40 @@ module AthenaHealth
   module Endpoints
     module Payments
 
+      def get_oneyear_contracts(practice_id:, patient_id:, department_id:, params: {})
+        @api.call(
+          endpoint: "#{practice_id}/patients/#{patient_id}/collectpayment/oneyear",
+          method: :get,
+          params: params.merge( departmentid: department_id )
+        )
+      end
+
+      def get_singleappointment_contracts(practice_id:, patient_id:, department_id:, params: {})
+        @api.call(
+          endpoint: "#{practice_id}/patients/#{patient_id}/collectpayment/singleappointment",
+          method: :get,
+          params: params.merge( departmentid: department_id )
+        )
+      end
+
+      def card_on_file(practice_id:, patient_id:, department_id:, body: {})
+		@api.call(
+	    	endpoint: "#{practice_id}/patients/#{patient_id}/collectpayment/storedcard",
+	    	method: :post,
+	    	body: body.merge(
+	      		departmentid: department_id
+	    	)
+		)
+      end
+
+      def get_stored_cards(practice_id:, patient_id:, department_id:, params: {})
+        @api.call(
+          endpoint: "#{practice_id}/patients/#{patient_id}/collectpayment/storedcard",
+          method: :get,
+          params: params.merge( departmentid: department_id )
+        )
+      end
+
       def get_receipt(practice_id:, patient_id:, epayment_id:, params: {})
         @api.call(
             endpoint: "#{practice_id}/patients/#{patient_id}/receipts/#{epayment_id}",
