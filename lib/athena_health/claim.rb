4127220707b17b3d44c81b8ee3d4d3a5abd84c29
rkiller <rogerkiller@gmail.com>
2018-09-13 16:15:21 -0700

    Adding ability to receive open balance/claim info through patient.rb

--- lib/athena_health/claim.rb
+++ lib/athena_health/claim.rb
@@ -1,6 +1,6 @@
 module AthenaHealth
   class Claim < BaseModel
-    attribute :claimdetails, Array[ClaimDetails]
+    attribute :claimdetails, Array[ClaimDetail]
  	attribute :providergroupid, Integer
  	attribute :departmentids, String
   end
