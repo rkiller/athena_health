c590a524ce2c4c87f25af6432a6c177aa2a30c2f
rkiller <rogerkiller@gmail.com>
2019-03-14 12:10:20 -0700

    Added Subscriptions Endpoint

--- lib/athena_health/endpoints/subscriptions.rb
+++ lib/athena_health/endpoints/subscriptions.rb
@@ -2,8 +2,23 @@ module AthenaHealth
   module Endpoints
     module Subscriptions
       
-      
+      #https://api.athenahealth.com/v1/15000/claims/changed/subscription?showadditionalevents=XXX
+      def get_claim_events(practice_id:, params: {})
+        response = @api.call(
+          endpoint: "#{practice_id}/claims/changed/subscription",
+          method: :get,
+          params: params
+        )
+      end
 
+      #https://api.athenahealth.com/v1/15000/claims/changed/subscription
+	  def create_patient(practice_id:, params: {})
+        @api.call(
+          endpoint: "#{practice_id}/claims/changed/subscription",
+          method: :post,
+          body: params
+        )
+      end
     end
   end
 end
