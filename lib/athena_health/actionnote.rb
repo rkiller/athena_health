cd7a82adc44e491d637b252f306c8215e32a7982
rkiller <rogerkiller@gmail.com>
2021-10-30 15:24:45 -0700

    Adding ability to set and get actionnotes on lab results

--- lib/athena_health/actionnote.rb
+++ lib/athena_health/actionnote.rb
@@ -1,11 +1,11 @@
 module AthenaHealth
   class Actionnote < BaseModel
-    attribute :actionnote,      String
-    attribute :priority,        Integer
-    attribute :assignedto,      String
-    attribute :createddatetime  String
-    attribute :status           String
-    attribute :patientid        Integer
-    attribute :createdby        String
+    attribute :actionnote,        String
+    attribute :priority,          Integer
+    attribute :assignedto,        String
+    attribute :createddatetime,   String
+    attribute :status,            String
+    attribute :patientid,         Integer
+    attribute :createdby,         String
   end
 end
