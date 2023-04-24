4ba425f1756eca26c9bf0c1b9faee57ad2325d77
rkiller <rogerkiller@gmail.com>
2023-04-24 15:39:10 -0700

    Added get and create employer functions

---
+++ lib/athena_health/endpoints/employers.rb
@@ -0,0 +1,38 @@
+module AthenaHealth
+	module Endpoints
+		module Employers
+
+			# {
+			#   "id": "string",
+			#   "fax": "string",
+			#   "zip": "string",
+			#   "city": "string",
+			#   "name": "string",
+			#   "notes": "string",
+			#   "phone": "string",
+			#   "state": "string",
+			#   "address": "string",
+			#   "ordering": "string"
+			# }
+			def find_employer(params: {})
+				response = @api.call(
+					endpoint: "#{practice_id}/employers",
+					method: :get,
+					params: params
+				)
+			end
+
+			# Returns:
+			# {
+			#   "employerid": 0
+			# }
+			def create_employer(params: {})
+				response = @api.call(
+					endpoint: "#{practice_id}/employers",
+					method: :post,
+					body: params
+				)
+			end
+		end
+	end
+end
