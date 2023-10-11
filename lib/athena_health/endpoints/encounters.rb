8fe32049132d8293c4ca319cfc292dcc9b156a91
rkiller <rogerkiller@gmail.com>
2023-10-11 08:52:22 -0700

    Updating encounter calls

--- lib/athena_health/endpoints/encounters.rb
+++ lib/athena_health/endpoints/encounters.rb
@@ -118,7 +118,7 @@ module AthenaHealth
         @api.call(
           endpoint:  "#{practice_id}/chart/encounter/#{encounter_id}/reviewofsystems",
           method: :put,
-          body: params
+          params: params
         )
       end
 
